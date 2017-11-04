1366345be8ec5af07cb4004d841fa969621bfc22
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-11-04 16:26:31 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,8 +4,9 @@ class Application
   def call(env)
     req = Rack::Request.new(env)
     resp = Rack::Response.new
-    binding.pry
+    
     if req.path.include? ("/item/")
+      binding.pry
     else
       resp.write "File not found"
       resp.status 404
