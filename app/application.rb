58782be9f5695cf26a2102a7bc8a49a26d19c7f9
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-11-04 16:28:34 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
   def call(env)
     req = Rack::Request.new(env)
     resp = Rack::Response.new
-    binding.pry
+    
     if req.path.include? ("/items/")
       binding.pry
     else
