a01bc38bd765b007c801df5cf523102713c94a22
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-11-04 16:37:04 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,9 +8,14 @@ class Application
     if req.path.include? ("/items/")
 
       search_item=req.path.split("items/").last
-      @@items.select {|item| item.name==search_item}
-        
-      binding.pry
+      found_item=@@items.select {|item| item.name==search_item}
+      if found_item
+      found_item[0].price
+      else
+      resp.write "Item not Found"
+      resp.status = 400
+    end
+        #binding.pry
     else
       resp.write "Route not found"
       resp.status = 404
